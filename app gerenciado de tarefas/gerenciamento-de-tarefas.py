a4509d3fadcd31af753e675f8c3e0a51cf8d46a0
Raf4el777 <202310591@uesb.edu.br>
2023-10-17 13:00:05 -0300

    ruby

--- app gerenciado de tarefas/gerenciamento-de-tarefas.py
+++ app gerenciado de tarefas/gerenciamento-de-tarefas.py
@@ -2,10 +2,41 @@
 #adicionar, editar e excluir tarefas.
 #Você pode implementar recursos como categorização, data de vencimento e prioridade
 from tkinter import *
+from tkinter import ttk
+import bancotarefas
+from tkinter import messagebox
+
+
+def add():
+    import info_tarefas
+    info_tarefas.inserir()
+
+def popular():
+    tv.delete(*tv.get_children())
+    vquery="SELECT * FROM tabela order by data"
+    linhas=bancotarefas.dql(vquery)
+    for i in linhas:
+        tv.insert("","end",values=i)    
+
+
+def pesquisar():
+    tv.delete(*tv.get_children())
+    vquery="SELECT * FROM tabela WHERE descricao LIKE '%"+vpesq.get()+"%'"
+    linhas=bancotarefas.dql(vquery)
+    for i in linhas:
+        tv.insert("","end",values=i)
+
+
+def deletar():
+    try:
+        itemSele=tv.selection()[0]
+        tv.delete(itemSele)
+    except:
+        messagebox.showinfo(title="ERRO", message="Selecione o elemento para deleta-lo")    
 
 App = Tk()
 App.title("Gerenciador de Tarefas")
-App.geometry('1480x720')
+App.geometry('700x400')
 App.configure(background='#dde')
 
 fr_quadro=Frame(App,borderwidth=1,relief="sunken",background="#fff")
@@ -14,13 +45,30 @@ fr_quadro.place(x=5,y=5,width=150,height=700)
 
 Label(fr_quadro,text="menu de terefas",font=("Arial", 15)).pack()
 
-btn_Menu=Button(fr_quadro,text="Adicionar",font=("Arial", 20),relief="flat",background="#fff")
+btn_Menu=Button(fr_quadro,text="Adicionar",font=("Arial", 20),relief="flat",background="#fff",command=add)
 btn_Menu.pack()
 
-btn_Menu=Button(fr_quadro,text="Editar",font=("Arial", 20),relief="flat",background="#fff")
+
+
+btn_Menu=Button(fr_quadro,text="Excluir",font=("Arial", 20),relief="flat",background="#fff",command=deletar)
 btn_Menu.pack()
 
-btn_Menu=Button(fr_quadro,text="Excluir",font=("Arial", 20),relief="flat",background="#fff")
-btn_Menu.pack()
+tarefas = LabelFrame(App,text="Tarefas",borderwidth=1,relief="flat")
+tarefas.place(x=200,y=20,width=500,height=250)
+tv=ttk.Treeview(tarefas,columns=('data','descricao','nome'),show="headings")
+tv.column('data',minwidth=0,width=100)
+tv.column('nome',minwidth=0,width=70)
+tv.column('descricao',minwidth=0,width=250)
+tv.heading('data',text="DATA")
+tv.heading('descricao',text="DESCRICAO")
+tv.heading('nome',text="NOME")
+tv.pack()
+
+pesquisar_tarefa = LabelFrame(App,text="Pesquisar tarefas",borderwidth=1,relief="flat")
+pesquisar_tarefa.place(x=200,y=290,width=450,height=75)
+Button(pesquisar_tarefa,text="Pesqisar",command=pesquisar).grid(column=1,row=0)
+vpesq=Entry(pesquisar_tarefa)
+vpesq.grid(column=0,row=0,)
+Button(pesquisar_tarefa,text="Mostrar todas",command=popular).grid(column=2,row=0)
 
 App.mainloop()
--- app gerenciado de tarefas/info_tarefas.py
+++ app gerenciado de tarefas/info_tarefas.py
@@ -2,21 +2,48 @@
 #adicionar, editar e excluir tarefas.
 #Você pode implementar recursos como categorização, data de vencimento e prioridade
 from tkinter import *
+import bancotarefas
+from tkinter import  messagebox
+
+
+def inserir():
+    if en_data.get() != "" and en_data.get() != "// | //" and en_tarefa.get('1.0', END) != "" and  en_fim.get() != "":
+        print("Dados gravados")
+        vdat = en_data.get()
+        vnome = en_tarefa.get('1.0', END)
+        vdesq = en_fim.get()
+        vquery = "INSERT INTO tabela (data, descricao, nome) VALUES ('"+vdat+"', '"+vdesq+"', '"+vnome+"')"
+        bancotarefas.dml(vquery) 
+    else:
+        print("ERRO")
+    ############
+    if en_data.get() == "" or en_fim.get() == "" or  en_tarefa.get('1.0', END) == "":
+        messagebox.showinfo(title="ERRO", message="campo não preencido")
+        return
+    en_data.delete(0,END)
+    en_tarefa.delete('1.0',END)
+    en_fim.delete(0,END)
+    en_data.focus()
 
 App = Tk()
 App.title("Gerenciador de Tarefas")
 App.geometry('350x250')
 App.configure(background='#dde')
 
-Label(App,text="Data de inicio").place(x=10,y=5)
-en_inicio=Entry(App)
-en_inicio.place(x=10,y=30,width=200,height=20)
-Label(App,text="Data de termino").place(x=10,y=55)
+vdata = StringVar()
+Label(App,text="Data de inicio | data de termino").place(x=10,y=5)
+en_data=Entry(App,textvariable=vdata)
+en_data.place(x=10,y=30,width=200,height=20)
+vdata.set("// | //")
+
+Label(App,text="Nome da tarefa").place(x=10,y=55)
 en_fim=Entry(App)
 en_fim.place(x=10,y=80,width=200,height=20)
-Label(App,text="Descrição da tarefa").place(x=10,y=105)
+Label(App,text="Descricao da tarefa").place(x=10,y=105)
 en_tarefa = Text(App)
 en_tarefa.place(x=10,y=130,width=300,height=100)
 
+btn = Button(App,text="Inserir",command=inserir)
+btn.place(x=250,y=50)
 
 App.mainloop()
